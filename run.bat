b4c8c0d6c1b4e0073be27d7dfc589131023d3cdc
notime <kordian.kuriata@protonmail.com>
2024-09-06 14:27:53 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -8,15 +8,14 @@ pause
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 timeout 8
 
-choco install choco install brave -y
+choco install brave -y
 choco install superf4 -y
 choco install leagueoflegendseune -y
 choco install blitz.gg -y
 choco install qbittorrent -y
-choco install stremio -y
 choco install 7zip -y
 choco install discord -y
-choco install treesize -y
+choco install treesizefree -y
 choco install everything -y
 choco install th-ch-youtube-music -y
 choco install hwinfo -y
@@ -27,7 +26,6 @@ choco install git.install --params "/GitAndUnixToolsOnPath" -y
 choco install evernote -y
 choco install crystaldiskmark -y
 choco install crystaldiskinfo -y
-choco install equalizerapo -y
 choco install protonpass -y
 
 pause
@@ -38,5 +36,7 @@ start "" https://store.steampowered.com/about/?l=polish/
 start "" https://www.argusmonitor.com/
 start "" https://sourceforge.net/projects/equalizerapo/
 start "" https://sourceforge.net/projects/peace-equalizer-apo-extension/
+start "" https://vencord.dev/
+start "" https://www.stremio.com/downloads
 start "" https://www.tomshardware.com/how-to/disable-windows-web-search/
 start "" https://answers.microsoft.com/en-us/windows/forum/all/restore-old-right-click-context-menu-in-windows-11/a62e797c-eaf3-411b-aeec-e460e6e5a82a/
